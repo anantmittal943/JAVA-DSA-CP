cbdca5985fa9581485439117cf75f10c856d73ae
Anant Mittal <anantmittal943@gmail.com>
2025-09-26 09:51:10 +0530

    26-09

---
+++ dsa-cp/src/main/java/com/anantmittal/dsa_cp/OOPJ_Class/Q1_26_09.java
@@ -0,0 +1,55 @@
+package com.anantmittal.dsa_cp.OOPJ_Class;
+
+class LowMarksException extends Exception {
+    public LowMarksException(String message) {
+        super(message);
+    }
+}
+
+class Student {
+    private final String name;
+    private final int marks;
+
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+        System.out.println("Student object created for " + this.name + ".");
+    }
+
+    public void display() throws LowMarksException {
+        System.out.println("\n--- Displaying Student Data ---");
+        System.out.println("Name: " + this.name);
+        System.out.println("Marks: " + this.marks);
+
+        if (this.marks < 40) {
+            throw new LowMarksException("Marks are below 40%. The student has failed.");
+        } else {
+            System.out.println("Result: Passed");
+        }
+        System.out.println("-----------------------------");
+    }
+}
+public class Q1_26_09 {
+    public static void main(String[] args) {
+        System.out.println("--- Program Start ---");
+
+        try {
+            Student student1 = new Student("Alice", 85);
+            student1.display();
+        } catch (LowMarksException e) {
+            System.err.println("\nCaught Exception: " + e.getMessage());
+        }
+
+        System.out.println("\n--- Next Student ---");
+
+        try {
+            Student student2 = new Student("Bob", 38);
+            student2.display();
+        } catch (LowMarksException e) {
+            System.err.println("\nCaught Exception for Bob: " + e.getMessage());
+            System.err.println("Further processing for this student might be required.");
+        }
+
+        System.out.println("\n--- Program End ---");
+    }
+}
---
+++ dsa-cp/src/main/java/com/anantmittal/dsa_cp/OOPJ_Class/Q2_26_09.java
@@ -0,0 +1,7 @@
+package com.anantmittal.dsa_cp.OOPJ_Class;
+
+public class Q2_26_09 {
+    public static void main(String[] args) {
+        System.out.println("Q2_26_09");
+    }
+}
